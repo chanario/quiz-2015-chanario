6367efef71809bf4e1f4982e007f60b79259eac5
chanario <chanario@gmail.com>
2015-06-21 18:50:02 +0100

    Despliegue DB en Heroku

--- models/models.js
+++ models/models.js
@@ -1,35 +1,54 @@
 //Se requiere la variable de entorno path
 var path = require('path');
 
+// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite   DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
+
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
-// Usar BBDD SQLite con new Sequelize(database, [username=null], [password=null], [options={}])
+// Usar BBDD SQLite o Postgres con new Sequelize(database, [username=null], [password=null], [options={}])
 // (Aquí es donde se conecta la BD: quiz.sqlite)
-var sequelize = new Sequelize(null, null, null,
-                       {dialect: "sqlite", storage: "quiz.sqlite"}
-                    );
+var sequelize = new Sequelize(DB_name, user, pwd,
+  { dialect:  protocol,
+    protocol: protocol,
+    port:     port,
+    host:     host,
+    storage:  storage,  // solo SQLite (.env)
+    omitNull: true      // solo Postgres
+  }
+);
 
 // Importar la definicion de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
 
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
 
 // sequelize.sync([options={}]) crea e inicializa tabla de preguntas en DB
 sequelize.sync()
-  .success(function() {
-        // success(..) ejecuta el manejador una vez creada la tabla
+  .then(function() {
+        // then(..) ejecuta el manejador una vez creada la tabla
         Quiz.count()
-          .success(function (count){
+          .then(function (count){
               if(count === 0) {   // la tabla se inicializa solo si está vacía
                 Quiz.create(
                            { pregunta: 'Capital de Italia',
                 	           respuesta: 'Roma'
                 	         }
                 )
-                  .success(function(){console.log('Base de datos inicializada')});
+                  .then(function(){console.log('Base de datos inicializada')});
               }; // if count
           }
-        ); // success (function (count)...)
+        ); // then (function (count)...)
       }
-  ); // success (function...)
+  ); // then (function...)
