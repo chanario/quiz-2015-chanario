f766702f075b50bad7a5d5d6d715ac46ad75f6dd
chanario <chanario@gmail.com>
2015-06-22 09:20:30 +0100

    Lista de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,27 +1,30 @@
 // Ahora lo hacemos a través del modelo concreto definido en models.js a través de Quiz
 var models = require('../models/models.js');
 
-// GET /quizes/question
-exports.question = function(req, res) {
-// findAll([options]) donde options es una búsqueda SQL (si está vacía las encuentra todas)
-  models.Quiz.findAll()
-    .success(function(quiz) {
-        // renderiza Views/quizes/question.ejs y le pasa la primera pregunta que encuentra.
-        res.render('quizes/question', { pregunta: quiz[0].pregunta});
-      }
-    )
+// GET /quizes
+exports.index = function(req, res) {
+  models.Quiz.findAll().then(function(quizes) {
+    res.render('quizes/index.ejs', { quizes: quizes});
+  })
 };
 
-// GET /quizes/answer
+// GET /quizes/:id
+exports.show = function(req, res) {
+  models.Quiz.find(req.params.quizId).then(function(quiz) {
+    res.render('quizes/show', { quiz: quiz});
+  })
+};
+
+// GET /quizes/:id/answer
 exports.answer = function(req, res) {
-  models.Quiz.findAll()
-    .success(function(quiz) {
+  models.Quiz.find(req.params.quizId)
+    .then(function(quiz) {
         // renderiza Views/quizes/answer.ejs con correcto o incorrecto
-        if (req.query.respuesta === quiz[0].respuesta) {
-          res.render('quizes/answer', { respuesta: 'Correcto' });
+        if (req.query.respuesta === quiz.respuesta) {
+          res.render('quizes/answer', { quiz: quiz, respuesta: 'Correcto' });
         } else {
-          res.render('quizes/answer', { respuesta: 'Incorrecto'});
+          res.render('quizes/answer', { quiz: quiz, respuesta: 'Incorrecto'});
         }
       }
-    )
+    ) // then
 };
