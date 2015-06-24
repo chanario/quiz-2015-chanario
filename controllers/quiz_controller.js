603b7c24e798239f3369b56ad7ef78e217d41bbf
chanario <chanario@gmail.com>
2015-06-24 16:22:54 +0100

    Validar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,16 +1,16 @@
 // Ahora lo hacemos a través del modelo concreto definido en models.js a través de Quiz
 var models = require('../models/models.js');
 
-// Autoload - factoriza el código si ruta incluye :quizId
+// Autoload :id
 exports.load = function(req, res, next, quizId) {
   models.Quiz.find(quizId).then(
     function(quiz) {
       if (quiz) {
         req.quiz = quiz;
         next();
-      } else { next(new Error('No existe quizId=' + quizId)); }
+      } else { next(new Error('No existe quizId=' + quizId)) }
     }
-  ).catch(function(error) { next(error);});
+  ).catch(function(error) { next(error)});
 };
 
 // GET /quizes con listado de preguntas filtradas
@@ -27,15 +27,15 @@ exports.index = function(req, res) {
 
   models.Quiz.findAll(miQuery).then(
     function(quizes) {
-      res.render('quizes/index.ejs', { quizes: quizes, search: req.query.search });
+      res.render('quizes/index.ejs', { quizes: quizes, search: req.query.search, errors: [] });
     }
-  ).catch(function(error) { next(error);})
+  ).catch(function(error) { next(error)})
 };
 
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-  res.render('quizes/show', { quiz: req.quiz});
+  res.render('quizes/show', { quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/:id/answer
@@ -44,25 +44,55 @@ exports.answer = function(req, res) {
   if (req.query.respuesta === req.quiz.respuesta) {
     resultado = 'Correcto';
   }
-  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});
 };
 
 
 // GET /quizes/new
 exports.new = function(req, res) {
-  var quiz = models.Quiz.build(
+  var quiz = models.Quiz.build(  // Crea objeto quiz
     {pregunta: "Pregunta", respuesta: "Respuesta"}
   );
 
-  res.render('quizes/new', {quiz: quiz});
+  res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 // POST /quizes/create
 exports.create = function(req, res) {
   var quiz = models.Quiz.build( req.body.quiz );
 
-// guarda en DB los campos pregunta y respuesta de quiz
-  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-    res.redirect('/quizes');
-  })   // res.redirect: Redirección HTTP a lista de preguntas
+// Esto es lo que está en el SERVIDOR:
+//  quiz.
+//    .validate()
+//    .then(
+//      function(err){
+//        if (err) {
+//          res.render('quizes/new', {quiz: quiz, errors: err.errors});
+//        } else {
+//            quiz
+//              .save({fields: ["pregunta", "respuesta"]})
+//              .then( function(){ res.redirect('/quizes')})
+//          }
+//      }
+//    );
+//};
+
+
+// Esto es lo que se propone en el formamos
+
+  var errors = quiz.validate();//ya qe el objeto errors no tiene then(
+
+  if (errors) {
+    var i=0;
+    var errores= new Array(); //se convierte en [] con la propiedad message por compatibilida con layout
+
+    for (var prop in errors) errores[i++]= { message: errors[prop] };
+
+    res.render('quizes/new', {quiz: quiz, errors: errores});
+
+  } else {
+      quiz // save: guarda en DB campos pregunta y respuesta de quiz
+        .save({fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes')}) ; // res.redirect: Redirección HTTP a lista de preguntas
+    }
 };
--- models/quiz.js
+++ models/quiz.js
@@ -1,9 +1,17 @@
-// Definicion del modelo de Quiz mediante sequelize.define
+// Definicion del modelo de Quiz mediante sequelize.define y con validación
 
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('Quiz',
-            { pregunta:  DataTypes.STRING,
-              respuesta: DataTypes.STRING,
-            }
-         );
-}
+  return sequelize.define(
+    	'Quiz',
+      { pregunta: {
+          type: DataTypes.STRING,
+          validate: { notEmpty: {msg: "-> Falta Pregunta"}}
+        },
+        respuesta: {
+          type: DataTypes.STRING,
+          validate: { notEmpty: {msg: "-> Falta Respuesta"}}
+        }
+      }
+    );
+  }
+  
